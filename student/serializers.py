f330382800a31e8def9e22ea8ff526d1e951a32c
Muhidn <mahmoudmuhidin6@gamil.com>
2025-07-06 07:55:23 +0300

    last commit 06/07

--- student/serializers.py
+++ student/serializers.py
@@ -29,36 +29,29 @@ class StudentSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         user_data = validated_data.pop('user', None)
         if user_data:
+            # Manually update user fields to avoid validation issues
             user = instance.user
-            # Prevent updating username and email
-            user_data.pop('username', None)
-            user_data.pop('email', None)
             for attr, value in user_data.items():
                 if attr == 'password' and value:
                     user.set_password(value)
-                elif attr != 'password':
+                elif attr in ['username', 'email']:
+                    # Only update if the value is actually different
+                    if getattr(user, attr) != value:
+                        # Check for uniqueness manually
+                        from users.models import User
+                        if User.objects.filter(**{attr: value}).exclude(pk=user.pk).exists():
+                            raise serializers.ValidationError({
+                                'user': {attr: [f'user with this {attr} already exists.']}
+                            })
+                        setattr(user, attr, value)
+                else:
                     setattr(user, attr, value)
             user.save()
+        
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
         return instance
-
-    def validate(self, data):
-        user_data = data.get('user')
-        if user_data and self.instance:
-            user = self.instance.user
-            username = user_data.get('username')
-            if username and username != user.username:
-                from users.models import User
-                if User.objects.filter(username=username).exclude(pk=user.pk).exists():
-                    raise serializers.ValidationError({'user': {'username': ['user with this username already exists.']}})
-            email = user_data.get('email')
-            if email and email != user.email:
-                from users.models import User
-                if User.objects.filter(email=email).exclude(pk=user.pk).exists():
-                    raise serializers.ValidationError({'user': {'email': ['user with this email already exists.']}})
-        return data
 
 class LectureSerializer(serializers.ModelSerializer):
     class Meta:
